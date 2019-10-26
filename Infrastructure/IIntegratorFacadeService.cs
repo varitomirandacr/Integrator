22b1bf2cb049d47d425ca9a903474ef1944f246a
Alvaro Andrei Miranda <varitomiranda@hotmail.com>
2019-10-26 15:12:18 -0600

    INT-004 Adding Infrastructure project

---
+++ Infrastructure/IIntegratorFacadeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    interface IIntegratorFacadeService
+    {
+        void ExecuteServices<T>(List<T> services);
+    }
+}
